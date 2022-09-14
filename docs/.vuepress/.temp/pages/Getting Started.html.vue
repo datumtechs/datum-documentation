b95567cdb23f032123988df5bca7a563ec4f80ad
Gavin520 <meiyan532484710@163.com>
2022-09-14 15:41:37 +0800

    Signed-off-by: Gavin520 <meiyan532484710@163.com>

--- docs/.vuepress/.temp/pages/Getting Started.html.vue
+++
@@ -1,8 +0,0 @@
-<template><div><h1 id="开始准备" tabindex="-1"><a class="header-anchor" href="#开始准备" aria-hidden="true">#</a> 开始准备</h1>
-<ul>
-<li>准备好自己需要获得商业价值或者需要进入Web3世界流动的原始数据</li>
-<li>仔细观看datum api 使用文档</li>
-</ul>
-</div></template>
-
-
